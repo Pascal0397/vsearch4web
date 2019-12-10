531e9deb2bbbe70ddb1d6dd9f0c2e9ab3f2b3210
Pascal0397 <53826090+Pascal0397@users.noreply.github.com>
2019-12-10 10:29:12 +0100

    Uploaded Version1

---
+++ vsearch4web/HeadsFirst/Lists, Tuples, Dictionary, Set.py
@@ -0,0 +1,12 @@
+prices = []  # Empty List
+
+temps = [ 32.0, 212.0, 0.0, 81.6, 100.0, 45.3 ]  # List with float numbers
+
+words = [ 'hello', 'world' ]  # List with strings
+
+car_details = [ 'Toyota', 'RAV4', 2.2, 60807 ]  # Mixed List with Strings, Float and integers
+
+everything = [ prices, temps, words, car_details ]  # Lists inside a List
+
+odds_and_ends = [ [ 1, 2, 3 ], [ 'a', 'b', 'c' ],  # Lists created in an another List
+                  [ 'One', 'Two', 'Three' ] ]
